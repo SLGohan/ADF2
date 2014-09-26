182d1f7dcc3ff0de97fb97ed11c547465bff8b43
SLGohan <hoangcuong3012@gmail.com>
2014-09-26 08:52:52 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @author SLGohan
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+    	MyThread t1 = new MyThread("My Thread");
+    	t1.getName();
+    	t1.reName("Lap di");
+    }
+}
